da3133ef9d287d0def1d4e75978811e5b3595209
kgminhtet <kgminhtet.dev@gmail.com>
2024-12-16 10:30:20 +0630

    Refactoring test cases

--- test/sorting_test.go
+++ test/sorting_test.go
@@ -2,27 +2,23 @@ package test
 
 import (
 	"github.com/kgminhtet-dev/data_structures_and_algorithms/day3"
-	"reflect"
 	"testing"
 )
 
 func TestSorting(t *testing.T) {
-	testcases := []struct {
-		input    []int
-		expected []int
-	}{
-		{[]int{1, 2, 3, 4, 5}, []int{1, 2, 3, 4, 5}},
-		{[]int{5, 4, 3, 2, 1}, []int{1, 2, 3, 4, 5}},
-		{[]int{1, 1, 1, 1, 1}, []int{1, 1, 1, 1, 1}},
-		{[]int{1}, []int{1}},
-		{[]int{}, []int{}},
+	testcases := [][]int{
+		[]int{1, 2, 3, 4, 5},
+		[]int{5, 4, 3, 2, 1},
+		[]int{1, 1, 1, 1, 1},
+		[]int{1},
+		[]int{},
 	}
 
 	t.Run("Bubble Sort", func(t *testing.T) {
 		for _, tc := range testcases {
-			result := day3.BubbleSort(tc.input)
-			if !reflect.DeepEqual(result, tc.expected) {
-				t.Errorf("Expected %v, but got %v\n", tc.expected, result)
+			result := day3.BubbleSort(tc)
+			if !isArraySorted(result) {
+				t.Errorf("Result should be sorted %v", result)
 			}
 		}
 	})

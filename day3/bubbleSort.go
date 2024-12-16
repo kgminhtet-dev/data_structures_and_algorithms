7131c6f9cff0c79da90435a559a73cb9e132c35e
kgminhtet <kgminhtet.dev@gmail.com>
2024-12-16 08:59:07 +0630

    bubble sort algorithim and testing it

---
+++ day3/bubbleSort.go
@@ -0,0 +1,17 @@
+package day3
+
+func bubbleSort(input []int) []int {
+	if len(input) == 0 || len(input) == 1 {
+		return input
+	}
+
+	for i := 1; i < len(input); i++ {
+		for j := 0; j < len(input)-i; j++ {
+			if input[j] > input[j+1] {
+				input[j], input[j+1] = input[j+1], input[j]
+			}
+		}
+	}
+
+	return input
+}
